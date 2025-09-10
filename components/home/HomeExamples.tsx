e9fca07bedd5584c176aedb240f438a9d05f7815
Christian Zanchetta <>
2025-09-10 10:00:45 +0200

    Added AI Demo example

--- components/home/HomeExamples.tsx
+++ components/home/HomeExamples.tsx
@@ -1,3 +1,4 @@
+import { House } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -8,8 +9,8 @@ export default function HomeExamples() {
         <span className="text-sm text-gray-400">Demo example</span>
         <span className="block min-h-[1px] max-h-[1px] w-8 bg-gray-400"></span>
       </div>
-      <div className="flex flex-col md:flex-row items-center gap-6 md:gap-12">
-        <div className="flex gap-4">
+      <div className="flex flex-col md:flex-row items-center gap-6">
+        <div className='p-4 shadow-2xs hover:shadow-lg rounded-md transition-shadow duration-300 ease-in-out'>
           <Link href="https://fisevents.com" target="_blank">
             <Image
               src="/images/fisevents_logo.png"
@@ -19,6 +20,13 @@ export default function HomeExamples() {
               height={150}
               className="rounded-md"
             />
+          </Link>
+        </div>
+        <div className="border-r-2 hidden md:block">&nbsp;</div>
+        <div className='p-4 shadow-2xs hover:shadow-lg rounded-md transition-shadow duration-300 ease-in-out'>
+          <Link href="https://fis-apart.vercel.app/" target="_blank" className="flex flex-col items-center">
+            <House className='w-10 h-10 text-blue-600' />
+            <span className='text-violet-800'>AI Demo</span>
           </Link>
         </div>
       </div>
